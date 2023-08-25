c848b3f54262607ad11d6af2f1c347f0ffda722d
Sundram Yadav <hot2shortcut@gmail.com>
2023-08-25 09:12:03 +0530

    added code

--- SeleniumDemo/src/HelloWorld.java
+++ SeleniumDemo/src/HelloWorld.java
@@ -1,10 +1,27 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-
+		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Desktop\\ChromeDriver\\chromedriver.exe");
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		driver.findElement(By.xpath("//textarea[@class=\"gLFyf\"]")).sendKeys("selenium");
+		List<WebElement> listBoxElements = driver.findElements(By.xpath("//ul[@role='listbox']/li"));
+		for(WebElement listBox:listBoxElements) {
+			System.out.println(listBox.getText());
+		}
+		driver.quit();
+		Thread.sleep(10000); 
+		driver.quit();
+		System.out.println(driver.findElement(By.xpath("//img[@class='lnXdpd']")).getText());
 	}
 
 }
