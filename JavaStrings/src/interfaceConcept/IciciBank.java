ab8490433fa785d10a609b24841ff3bd3eafa8fe
Sundram Yadav <hot2shortcut@gmail.com>
2023-11-08 20:19:50 +0530

    add

--- JavaStrings/src/interfaceConcept/IciciBank.java
+++ JavaStrings/src/interfaceConcept/IciciBank.java
@@ -1,6 +1,6 @@
 package interfaceConcept;
 
-public class ICICI implements Bank {
+public class IciciBank implements Bank {
 	int money=(int) (Math.random()*1000);
 	int min = 1;
     int max = 100000000;
@@ -8,7 +8,7 @@ public class ICICI implements Bank {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ICICI iciciClass=new ICICI();
+		IciciBank iciciClass=new IciciBank();
 		iciciClass.addAccount();		
 		iciciClass.credit();
 		iciciClass.debit();
