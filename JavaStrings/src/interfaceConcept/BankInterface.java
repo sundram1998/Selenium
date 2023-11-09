48da241e78da83bd67aea08c767918b55796bcc1
Sundram Yadav <hot2shortcut@gmail.com>
2023-11-09 09:17:33 +0530

    add

--- JavaStrings/src/interfaceConcept/BankInterface.java
+++ JavaStrings/src/interfaceConcept/BankInterface.java
@@ -1,12 +1,16 @@
 package interfaceConcept;
 
 public interface BankInterface {
-	//No static methods in interfaces
-	//No method body
-	// we can declare variable and they will be static by default in nature
-	//variable vales will not be changed
-	//We can not create object of interface
-	//Interface is abstract in nature
+	/*
+	  1:No static methods in interfaces
+	  2:No method body
+	  3:we can declare variable and they will be static by default in nature
+	  4:variable vales will not be changed
+	  5:We can not create object of interface
+	  6:Interface is abstract in nature
+	  7:It is used to achieve abstraction and support inheritance functionality
+	  8:It can be used to achieve loose couping
+	*/
 	
 	int min_bal=1000;
 	public void openAccount();
