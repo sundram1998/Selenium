495f987519e4424b3ecb3631e4726da303640cad
Sundram Yadav <hot2shortcut@gmail.com>
2024-05-26 21:06:57 +0530

    add

--- DSAGeekforGeeks/src/arrays/ArrayListMethods.java
+++ DSAGeekforGeeks/src/arrays/ArrayListMethods.java
@@ -18,7 +18,6 @@ public class ArrayListMethods {
 			System.out.print(iterator.next()+" ");
 		}
 	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		listMethods();
